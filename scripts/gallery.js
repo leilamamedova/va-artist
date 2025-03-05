465584cb5efe49920a2d3f70ae8d8aaaf00489dd
leilamamedova <leila.m26ova@gmail.com>
2025-03-05 23:40:51 +0400

    change gallery

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -2,20 +2,23 @@ var modal = document.querySelector('.modal-overlay');
 var modalImg = document.getElementById('full-image');
 var images = document.querySelectorAll('.img-container img');
 var closeBtn = document.querySelector('.close');
+
 function openModal(event) {
   modal.style.display = 'flex';
   modalImg.src = event.target.src;
 }
+
 function closeModal() {
   modal.style.display = 'none';
 }
 
 images.forEach(function (image) {
-  image.addEventListener('click', openModal);
+  image.addEventListener('pointerdown', openModal);
 });
-closeBtn.addEventListener('click', closeModal);
 
-modal.addEventListener('click', function (event) {
+closeBtn.addEventListener('pointerdown', closeModal);
+
+modal.addEventListener('pointerdown', function (event) {
   if (event.target === modal) {
     closeModal();
   }
