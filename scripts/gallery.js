958836a7c7251d2359d52dfc833b43cf0da92896
leilamamedova <leila.m26ova@gmail.com>
2025-03-05 23:44:01 +0400

    revert js changes

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -2,23 +2,20 @@ var modal = document.querySelector('.modal-overlay');
 var modalImg = document.getElementById('full-image');
 var images = document.querySelectorAll('.img-container img');
 var closeBtn = document.querySelector('.close');
-
 function openModal(event) {
   modal.style.display = 'flex';
   modalImg.src = event.target.src;
 }
-
 function closeModal() {
   modal.style.display = 'none';
 }
 
 images.forEach(function (image) {
-  image.addEventListener('pointerdown', openModal);
+  image.addEventListener('click', openModal);
 });
+closeBtn.addEventListener('click', closeModal);
 
-closeBtn.addEventListener('pointerdown', closeModal);
-
-modal.addEventListener('pointerdown', function (event) {
+modal.addEventListener('click', function (event) {
   if (event.target === modal) {
     closeModal();
   }
